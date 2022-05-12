c7450789aef9c53df309d2a9767991f7daee1417
Vo1maR <94230738+Vo1maR@users.noreply.github.com>
2022-05-12 22:25:18 +0300

    Update get_signal.c

--- get_signal.c
+++ get_signal.c
@@ -1,17 +1,19 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 
-static void sig_usr(int signo)
-{ 
-	if(signo == SIGUSR2)
-		printf("Process of Andrienko got signal");
+static void sig_usr(int signo) {
+     if (signo == SIGUSR2) {
+      printf("Process of Andrienko got signal SIGUSR2\n");
+     }
 }
 
-int main(void)
-{
-	printf("My PID = %d. Waiting for SIGUSR2 signal\n",getpid());
-	for ( ; ; )
-	pause();
-return EXIT_SUCCESS;
+int main (void) {
+    if(signal(SIGUSR2, sig_usr) == SIG_ERR) {
+        printf("Catch an error with SIGUSR2\n");
+    }
+    printf("My PID is %d. Waiting for SIGUSR2 signal\n", getpid());
+    for( ; ; )
+    pause();
+    return EXIT_SUCCESS;
 }
